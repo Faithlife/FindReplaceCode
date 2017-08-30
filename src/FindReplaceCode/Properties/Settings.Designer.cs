564c4a594b2a21c211da28fe4a6d765c7a2ba0b5
Ed Ball <ed.ball@faithlife.com>
2017-08-30 07:43:53 -0700

    Update Settings file.

--- src/FindReplaceCode/Properties/Settings.Designer.cs
+++ src/FindReplaceCode/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace FindReplaceCode.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "14.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.3.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -25,7 +25,8 @@ namespace FindReplaceCode.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("asax,cake,config,cs,csproj,fsd,json,md,nuspec,proto,ps1,settings,sln,xaml")]
+        [global::System.Configuration.DefaultSettingValueAttribute("asax,cake,config,cs,csproj,fsd,html,json,md,nuspec,proto,ps1,settings,sln,xaml,ym" +
+            "l")]
         public string FindReplaceFileContentExtensions {
             get {
                 return ((string)(this["FindReplaceFileContentExtensions"]));
