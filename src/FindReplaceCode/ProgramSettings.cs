53aec5faf5840839f507db20b59daba5e378ecab
Ed Ball <ed.ball@faithlife.com>
2019-03-12 08:17:03 -0700

    Change sealed class to static.

--- src/FindReplaceCode/ProgramSettings.cs
+++ src/FindReplaceCode/ProgramSettings.cs
@@ -2,7 +2,7 @@ using System.Collections.Generic;
 
 namespace FindReplaceCode
 {
-	public sealed class ProgramSettings
+	public static class ProgramSettings
 	{
 		public static readonly IReadOnlyCollection<string> FindReplaceFileContentExtensions = new HashSet<string>
 		{
